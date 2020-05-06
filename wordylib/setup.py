aecc1cf45f46cc201289542ba0320831d3445a1d
Davide Sarra <davide.sarra@gmail.com>
2020-05-06 23:12:17 +0100

    Add Alphabet

--- wordylib/setup.py
+++ wordylib/setup.py
@@ -14,6 +14,7 @@ setup(
     name=NAME,
     version=VERSION,
     include_package_data=True,
+    package_data={"wordylib": ["data/*"]},
     packages=find_packages(exclude=["tests"]),
     install_requires=get_requirements(),
 )
