d7c94d93cecdcbbd891df1806766d2dac11ed816
Davide Sarra <davide.sarra@gmail.com>
2020-05-16 22:27:55 +0100

    Test that WORDY header appears in web app

--- wordyweb/src/App.test.js
+++ wordyweb/src/App.test.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders wordy header', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const element = getByText(/wordy/i);
+  expect(element).toBeInTheDocument();
 });
