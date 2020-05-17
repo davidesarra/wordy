36993b24c87606f97f7819fc97181b3f486f5932
Davide Sarra <davide.sarra@gmail.com>
2020-05-17 08:31:11 +0100

    Add ShareGame component

--- wordyweb/src/components/game.component.jsx
+++ wordyweb/src/components/game.component.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import Axios from 'axios';
+import ShareGame from './shareGame.component'
 import PlayersSideBar from './playersSideBar.component'
 import LetterTiles from './letterTiles.component'
 
@@ -7,15 +8,16 @@ export default class Game extends Component {
     state = {
         players: []
     };
-    componentDidMount(){
+    componentDidMount() {
         Axios.get(`http://localhost:5000/api/v1/games/${this.props.location.state.gameId}/players`)
             .then(res => {
-                this.setState({players:res.data});
+                this.setState({ players: res.data });
             });
     }
-    render(){
+    render() {
         return (
             <React.Fragment>
+                <ShareGame gameId={this.props.location.state.gameId}></ShareGame>
                 <LetterTiles></LetterTiles>
                 <PlayersSideBar players={this.state.players}> </PlayersSideBar>
             </React.Fragment>
